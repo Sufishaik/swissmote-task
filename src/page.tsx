6cfc8c17bdaf87786c690205340ed0700fd8be3c
sufiyan <shaikhsufiyan174@gmail.com>
2024-08-25 15:34:23 +0530

    added color

--- src/page.tsx
+++ src/page.tsx
@@ -3,7 +3,7 @@ import ConnectWallet from "./components/ConnectWallet";
 
 const Page = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-800 p-4 sm:p-6 md:p-8 lg:p-12">
+    <div className="flex items-center justify-center min-h-screen bg-green-800 p-4 sm:p-6 md:p-8 lg:p-12">
       <header className="text-center bg-white p-4 sm:p-6 md:p-8 rounded-lg shadow-lg max-w-lg w-full mx-4 sm:mx-6 md:mx-8 lg:mx-auto">
         {/* Logo Image */}
         {/* <Image
